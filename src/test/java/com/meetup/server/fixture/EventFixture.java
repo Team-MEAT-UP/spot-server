cdcb13ce77a8bd5a7bbeee96f529413637f7d2a5
syjdjr <81100851+syjdjr@users.noreply.github.com>
2025-07-18 03:12:09 +0900

    [SPOT-121] 모임 생성 로직에 모임 시간과 날짜, 모임명을 추가하여 로직을 수정한다 (#129)
    * FEAT: (SPOT-121) 모임 생성 로직에 모임 시간과 날짜, 모임명을 추가하여 로직을 수정한다
    
    * FIX: (SPOT-121) 모임 생성 로직 변경에 따른 테스트 코드 수정한다
    
    * FEAT: (SPOT-121) 추가된 엔티티 필드에 대한 검증을 추가한다
    
    * FIX: (SPOT-121) NotBlank 추가 및 필드명 변경한다

--- src/test/java/com/meetup/server/fixture/EventFixture.java
+++ src/test/java/com/meetup/server/fixture/EventFixture.java
@@ -1,12 +1,32 @@
 package com.meetup.server.fixture;
 
 import com.meetup.server.event.domain.Event;
+import com.meetup.server.event.dto.request.EventRequest;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public class EventFixture {
 
     public static Event getEvent() {
         return Event.builder()
+                .eventName("모임핑")
+                .eventDateTime(LocalDateTime.parse("2025-10-01T10:00:00"))
                 .build();
     }
 
+    public static EventRequest getEventRequest() {
+        return new EventRequest(
+                "모임핑",
+                LocalDate.parse("2025-10-01"),
+                LocalTime.parse("10:00"),
+                "땡수팟",
+                "선정릉역 수인분당선",
+                "서울특별시 강남구 삼성동 111-114",
+                "서울특별시 강남구 선릉로 지하580",
+                127.043999,
+                37.510297
+        );
+    }
 }
