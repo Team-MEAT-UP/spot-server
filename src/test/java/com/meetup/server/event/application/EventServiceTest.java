00c1ea24d19348195c24abbdc8d3519400e5b894
syjdjr <81100851+syjdjr@users.noreply.github.com>
2025-07-18 03:12:09 +0900

    [SPOT-121] 모임 생성 로직에 모임 시간과 날짜, 모임명을 추가하여 로직을 수정한다 (#129)
    * FEAT: (SPOT-121) 모임 생성 로직에 모임 시간과 날짜, 모임명을 추가하여 로직을 수정한다
    
    * FIX: (SPOT-121) 모임 생성 로직 변경에 따른 테스트 코드 수정한다
    
    * FEAT: (SPOT-121) 추가된 엔티티 필드에 대한 검증을 추가한다
    
    * FIX: (SPOT-121) NotBlank 추가 및 필드명 변경한다

--- src/test/java/com/meetup/server/event/application/EventServiceTest.java
+++ src/test/java/com/meetup/server/event/application/EventServiceTest.java
@@ -1,12 +1,12 @@
 package com.meetup.server.event.application;
 
 import com.meetup.server.event.domain.Event;
+import com.meetup.server.event.dto.request.EventRequest;
 import com.meetup.server.event.dto.response.EventStartPointResponse;
 import com.meetup.server.event.persistence.EventRepository;
-import com.meetup.server.fixture.StartPointFixture;
+import com.meetup.server.fixture.EventFixture;
 import com.meetup.server.fixture.UserFixture;
 import com.meetup.server.startpoint.domain.StartPoint;
-import com.meetup.server.startpoint.dto.request.StartPointRequest;
 import com.meetup.server.startpoint.persistence.StartPointRepository;
 import com.meetup.server.support.IntegrationTestContainer;
 import com.meetup.server.user.domain.User;
@@ -36,55 +36,57 @@ class EventServiceTest extends IntegrationTestContainer {
     private UserRepository userRepository;
 
     private User user;
-    private StartPointRequest startPointRequest;
+    private EventRequest eventRequest;
     private UUID guestId;
 
     @BeforeEach
     void setUp() {
         user = userRepository.save(UserFixture.getUser());
-        startPointRequest = StartPointFixture.getStartPointRequest();
+        eventRequest = EventFixture.getEventRequest();
         guestId = UUID.randomUUID();
     }
 
     @Test
     void 비로그인_사용자가_이벤트를_생성한다() {
-        EventStartPointResponse eventStartPointResponse = eventService.createEvent(null, guestId, startPointRequest);
+        EventStartPointResponse eventStartPointResponse = eventService.createEvent(null, guestId, eventRequest);
 
         Optional<Event> optionalEvent = eventRepository.findById(eventStartPointResponse.eventId());
         assertThat(optionalEvent).isPresent();
 
         Optional<StartPoint> optionalStartPoint = startPointRepository.findById(eventStartPointResponse.startPointId());
         assertThat(optionalStartPoint).isPresent();
-        assertThat(eventStartPointResponse.username()).isEqualTo(startPointRequest.username());
+        assertThat(eventStartPointResponse.username()).isEqualTo(eventRequest.username());
         assertThat(optionalStartPoint.get().getStartPointId()).isEqualTo(eventStartPointResponse.startPointId());
-        assertThat(optionalStartPoint.get().getName()).isEqualTo(startPointRequest.startPoint());
+        assertThat(optionalStartPoint.get().getName()).isEqualTo(eventRequest.startPoint());
         assertThat(optionalStartPoint.get().getUser()).isNull();
-        assertThat(optionalStartPoint.get().getAddress().getAddress()).isEqualTo(startPointRequest.address());
-        assertThat(optionalStartPoint.get().getAddress().getRoadAddress()).isEqualTo(startPointRequest.roadAddress());
-        assertThat(optionalStartPoint.get().getLocation().getRoadLongitude()).isEqualTo(startPointRequest.longitude());
-        assertThat(optionalStartPoint.get().getLocation().getRoadLatitude()).isEqualTo(startPointRequest.latitude());
+        assertThat(optionalStartPoint.get().getAddress().getAddress()).isEqualTo(eventRequest.address());
+        assertThat(optionalStartPoint.get().getAddress().getRoadAddress()).isEqualTo(eventRequest.roadAddress());
+        assertThat(optionalStartPoint.get().getLocation().getRoadLongitude()).isEqualTo(eventRequest.longitude());
+        assertThat(optionalStartPoint.get().getLocation().getRoadLatitude()).isEqualTo(eventRequest.latitude());
         assertThat(optionalStartPoint.get().getGuestId()).isEqualTo(guestId);
     }
 
     @Transactional
     @Test
     void 로그인_사용자가_이벤트를_생성한다() {
-        EventStartPointResponse eventStartPointResponse = eventService.createEvent(user.getUserId(), null, startPointRequest);
+        EventStartPointResponse eventStartPointResponse = eventService.createEvent(user.getUserId(), null, eventRequest);
 
         Optional<Event> optionalEvent = eventRepository.findById(eventStartPointResponse.eventId());
         assertThat(optionalEvent).isPresent();
 
         Optional<StartPoint> optionalStartPoint = startPointRepository.findById(eventStartPointResponse.startPointId());
         assertThat(optionalStartPoint).isPresent();
-        assertThat(eventStartPointResponse.username()).isEqualTo(startPointRequest.username());
+        assertThat(eventStartPointResponse.username()).isEqualTo(eventRequest.username());
         assertThat(optionalStartPoint.get().getStartPointId()).isEqualTo(eventStartPointResponse.startPointId());
-        assertThat(optionalStartPoint.get().getName()).isEqualTo(startPointRequest.startPoint());
+        assertThat(optionalStartPoint.get().getName()).isEqualTo(eventRequest.startPoint());
         assertThat(optionalStartPoint.get().getUser()).isEqualTo(user);
-        assertThat(optionalStartPoint.get().getAddress().getAddress()).isEqualTo(startPointRequest.address());
-        assertThat(optionalStartPoint.get().getAddress().getRoadAddress()).isEqualTo(startPointRequest.roadAddress());
-        assertThat(optionalStartPoint.get().getLocation().getRoadLongitude()).isEqualTo(startPointRequest.longitude());
-        assertThat(optionalStartPoint.get().getLocation().getRoadLatitude()).isEqualTo(startPointRequest.latitude());
+        assertThat(optionalStartPoint.get().getAddress().getAddress()).isEqualTo(eventRequest.address());
+        assertThat(optionalStartPoint.get().getAddress().getRoadAddress()).isEqualTo(eventRequest.roadAddress());
+        assertThat(optionalStartPoint.get().getLocation().getRoadLongitude()).isEqualTo(eventRequest.longitude());
+        assertThat(optionalStartPoint.get().getLocation().getRoadLatitude()).isEqualTo(eventRequest.latitude());
         assertThat(optionalStartPoint.get().getGuestId()).isNull();
+        assertThat(optionalEvent.get().getEventName()).isEqualTo(eventRequest.eventName());
+        assertThat(optionalEvent.get().getEventDateTime()).isEqualTo(eventRequest.toDateTime());
     }
 
 }
