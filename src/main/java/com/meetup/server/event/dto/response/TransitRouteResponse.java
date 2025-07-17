19f49edd493a10e027aea502ca090145267ef65a
syjdjr <81100851+syjdjr@users.noreply.github.com>
2025-07-17 16:11:25 +0900

    [SPOT-121] 경로 없을 경우 NPE 처리 하기 (#122)
    * FIX: (#121) null 처리 코드 작성한다
    
    * FIX: (SPOT-121) optional of 제거 통해 변환 로직 개선한다

--- src/main/java/com/meetup/server/event/dto/response/TransitRouteResponse.java
+++ src/main/java/com/meetup/server/event/dto/response/TransitRouteResponse.java
@@ -27,6 +27,17 @@ public record TransitRouteResponse(
         int sectionTime //이동 소요 시간
 ) {
     public static List<TransitRouteResponse> from(OdsayTransitRouteSearchResponse response) {
+
+        if (response == null || response.data() == null || response.data().path() == null || response.data().path().isEmpty()) {
+            log.warn("[ODSAY] ROUTE IS EMPTY: {}", response);
+            return null;
+        }
+
+        if (response.data().path().getFirst() == null || response.data().path().getFirst().subPath() == null) {
+            log.warn("[ODSAY] ROUTE IS EMPTY : {}", response);
+            return null;
+        }
+
         return response.data().path().getFirst().subPath().stream()
                 .map(subPath -> {
                     String laneName = Optional.ofNullable(subPath.lane())
