b0ede5afd5ee15bed4d5de5f0817e25660de7b8b
syjdjr <syjdjr@naver.com>
2025-07-18 22:25:33 +0900

    FEAT: (SPOT-122) Response에 isTransit 을 제거한다

--- src/main/java/com/meetup/server/event/dto/response/EventStartPointResponse.java
+++ src/main/java/com/meetup/server/event/dto/response/EventStartPointResponse.java
@@ -20,10 +20,7 @@ public record EventStartPointResponse(
         UUID guestId,
 
         @Schema(description = "사용자명", example = "땡수팟")
-        String username,
-
-        @Schema(description = "대중교통 여부")
-        boolean isTransit
+        String username
 ) {
     public static EventStartPointResponse of(Event event, StartPoint startPoint) {
         return EventStartPointResponse.builder()
@@ -31,7 +28,6 @@ public record EventStartPointResponse(
                 .startPointId(startPoint.getStartPointId())
                 .guestId(startPoint.getGuestId())
                 .username(UsernameExtractor.extractDisplayName(startPoint))
-                .isTransit(startPoint.isTransit())
                 .build();
     }
 }
