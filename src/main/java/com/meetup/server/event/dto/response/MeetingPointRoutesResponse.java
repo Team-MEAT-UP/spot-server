28cad8d911d0e0def9c390608e6d053f6a6dae7c
anxi01 <tjdals2893@naver.com>
2025-07-18 16:37:46 +0900

    FEAT: (SPOT-130) 대중교통 선택 여부 기반으로 상세 경로를 조회한다

--- src/main/java/com/meetup/server/event/dto/response/MeetingPointRoutesResponse.java
+++ src/main/java/com/meetup/server/event/dto/response/MeetingPointRoutesResponse.java
@@ -1,8 +1,46 @@
 package com.meetup.server.event.dto.response;
 
+import com.meetup.server.event.domain.Event;
+import com.meetup.server.global.util.TimeUtil;
+import com.meetup.server.startpoint.domain.StartPoint;
+import com.meetup.server.startpoint.util.UsernameExtractor;
+
+import java.time.LocalDateTime;
+import java.util.Comparator;
 import java.util.List;
 
 public record MeetingPointRoutesResponse(
+        String eventName,
+        String eventDate,
+        String eventTime,
+        String eventMaker,
+        int peopleCount,
         List<MeetingPointRouteGroup> meetingPointRouteGroups
 ) {
+
+    public static MeetingPointRoutesResponse of(List<MeetingPointResult> meetingPointResults, List<MeetingPointRouteGroup> meetingPointRouteGroups) {
+        MeetingPointResult meetingPointResult = meetingPointResults.get(0);
+
+        Event event = meetingPointResult.event();
+        LocalDateTime eventDateTime = event.getEventDateTime();
+
+        List<StartPoint> startPoints = meetingPointResult.startPoints();
+
+        String eventMaker = startPoints.stream()
+                .sorted(Comparator.comparing(StartPoint::getCreatedAt))
+                .findFirst()
+                .map(UsernameExtractor::extractDisplayName)
+                .orElse(null);
+
+        int peopleCount = startPoints.size();
+
+        return new MeetingPointRoutesResponse(
+                event.getEventName(),
+                TimeUtil.formatAsDate(eventDateTime),
+                TimeUtil.formatAsTime(eventDateTime),
+                eventMaker,
+                peopleCount,
+                meetingPointRouteGroups
+        );
+    }
 }
