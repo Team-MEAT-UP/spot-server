9da4c05d9db86c06c5df6f7cf2cfea057ea577ef
SeongMin Han <123073840+anxi01@users.noreply.github.com>
2025-07-18 19:21:18 +0900

    [SPOT-129] 히스토리 조회 시 모임명, 모임날짜 및 시간을 추가로 반환하기 (#131)

--- src/main/java/com/meetup/server/global/util/TimeUtil.java
+++ src/main/java/com/meetup/server/global/util/TimeUtil.java
@@ -5,6 +5,7 @@ import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
@@ -12,11 +13,23 @@ public class TimeUtil {
 
     public static final ZoneId KST_ZONE_ID = ZoneId.of("Asia/Seoul");
 
+    private static final DateTimeFormatter YYYY_MM_DD_DOT_FORMATTER = DateTimeFormatter.ofPattern("yyyy.MM.dd");
+
+    private static final DateTimeFormatter HH_MM_FORMATTER = DateTimeFormatter.ofPattern("HH:mm");
+
     public static int calculateDaysAgo(LocalDateTime createdAt) {
         return (int) ChronoUnit.DAYS.between(createdAt, LocalDateTime.now(KST_ZONE_ID));
     }
 
     public static int calculateHoursAgo(LocalDateTime createdAt) {
         return (int) ChronoUnit.HOURS.between(createdAt, LocalDateTime.now(KST_ZONE_ID));
+    }
+
+    public static String formatAsDate(LocalDateTime dateTime) {
+        return dateTime.format(YYYY_MM_DD_DOT_FORMATTER);
+    }
+
+    public static String formatAsTime(LocalDateTime dateTime) {
+        return dateTime.format(HH_MM_FORMATTER);
     }
 }
