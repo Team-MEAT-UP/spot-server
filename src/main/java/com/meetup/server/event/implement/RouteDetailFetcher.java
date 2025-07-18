c33b66302c5780514b3682fb53686dcb2003d969
anxi01 <tjdals2893@naver.com>
2025-07-18 16:38:48 +0900

    REFACTOR: (SPOT-130) 경로를 가져올 수 없을 경우 null을 반환해 예외를 방지한다

--- src/main/java/com/meetup/server/event/implement/RouteDetailFetcher.java
+++ src/main/java/com/meetup/server/event/implement/RouteDetailFetcher.java
@@ -1,13 +1,9 @@
 package com.meetup.server.event.implement;
 
-import com.meetup.server.event.dto.response.DrivingInfoResponse;
-import com.meetup.server.event.dto.response.RouteResponse;
 import com.meetup.server.global.clients.kakao.mobility.KakaoMobilityResponse;
 import com.meetup.server.global.clients.odsay.OdsayTransitRouteSearchResponse;
-import com.meetup.server.startpoint.domain.StartPoint;
 import com.meetup.server.startpoint.exception.StartPointErrorType;
 import com.meetup.server.startpoint.exception.StartPointException;
-import com.meetup.server.startpoint.util.RouteExtractor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -19,23 +15,13 @@ public class RouteDetailFetcher {
 
     private final RouteApiCaller routeApiCaller;
 
-    public RouteResponse fetch(
-            StartPoint startPoint,
-            String startX, String startY, String endX, String endY
-    ) {
+    public OdsayTransitRouteSearchResponse fetchTransitRoute(String startX, String startY, String endX, String endY) {
         OdsayTransitRouteSearchResponse transitRoute = routeApiCaller.getTransitRoute(startX, startY, endX, endY);
+        return transitRoute;
+    }
+
+    public KakaoMobilityResponse fetchDrivingRoute(String startX, String startY, String endX, String endY) {
         KakaoMobilityResponse drivingRoute = routeApiCaller.getDrivingRoute(startX, startY, endX, endY);
-
-        if (transitRoute == null) {
-            throw new StartPointException(StartPointErrorType.ODSAY_ERROR);
-        }
-        if (drivingRoute == null) {
-            throw new StartPointException(StartPointErrorType.KAKAO_ERROR);
-        }
-
-        int transitTotalTime = RouteExtractor.extractValidTransitTotalTime(transitRoute);
-        int drivingTotalTime = RouteExtractor.extractValidDrivingTotalTime(DrivingInfoResponse.from(drivingRoute));
-
-        return RouteResponse.of(startPoint, startPoint.getUser(), transitRoute, drivingRoute, transitTotalTime, drivingTotalTime);
+        return drivingRoute;
     }
 }
