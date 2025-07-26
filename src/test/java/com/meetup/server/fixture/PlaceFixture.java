0416d8c64cd2c5b7346ab659cc72644f026c5a34
syjdjr <syjdjr@naver.com>
2025-07-26 10:00:41 +0900

    FEAT: (SPOT-106) CoordinateUtil을 사용해서 point 객체를 만든다

--- src/test/java/com/meetup/server/fixture/PlaceFixture.java
+++ src/test/java/com/meetup/server/fixture/PlaceFixture.java
@@ -1,5 +1,6 @@
 package com.meetup.server.fixture;
 
+import com.meetup.server.global.util.CoordinateUtil;
 import com.meetup.server.place.domain.Place;
 import com.meetup.server.place.domain.type.PlaceCategory;
 import com.meetup.server.startpoint.domain.type.Location;
@@ -8,8 +9,6 @@ import org.locationtech.jts.geom.GeometryFactory;
 import java.util.List;
 
 public class PlaceFixture {
-
-    private static final GeometryFactory geometryFactory = new GeometryFactory();
 
     public static Place getPlace() {
         return Place.builder()
@@ -22,7 +21,7 @@ public class PlaceFixture {
                 .openingHours(List.of())
                 .googleReviews(List.of())
                 .location(Location.of(37.5406181573079, 127.06798560729))
-                .point(geometryFactory.createPoint(new org.locationtech.jts.geom.Coordinate(127.06798560729, 37.5406181573079)))
+                .point(CoordinateUtil.createPoint(127.06798560729, 37.5406181573079))
                 .rawJson(null)
                 .build();
     }
