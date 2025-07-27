8be3a9640d53eea753e8b24b3bd14a0c1f13e922
SeongMin Han <123073840+anxi01@users.noreply.github.com>
2025-07-27 16:10:22 +0900

    [SPOT-77] 디스코드 웹훅을 사용한 서버 예외 알림 받기 (#135)
    * FEAT: (SPOT-77) 서버 예외 발생 시, Discord 웹훅을 사용해 알람을 받는다
    
    * FEAT: (SPOT-77) 디스코드 TimeStamp를 위한 formatter를 추가한다
    
    * FIX: (SPOT-77) Profile 사용 시 테스트환경에서 빈으로 생성되지 않아 env에 따라 분기처리한다

--- src/main/java/com/meetup/server/global/util/TimeUtil.java
+++ src/main/java/com/meetup/server/global/util/TimeUtil.java
@@ -5,6 +5,7 @@ import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
@@ -12,11 +13,17 @@ public class TimeUtil {
 
     public static final ZoneId KST_ZONE_ID = ZoneId.of("Asia/Seoul");
 
+    private static final DateTimeFormatter YYYY_MM_DD_HH_MM_SS_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
     public static int calculateDaysAgo(LocalDateTime createdAt) {
         return (int) ChronoUnit.DAYS.between(createdAt, LocalDateTime.now(KST_ZONE_ID));
     }
 
     public static int calculateHoursAgo(LocalDateTime createdAt) {
         return (int) ChronoUnit.HOURS.between(createdAt, LocalDateTime.now(KST_ZONE_ID));
+    }
+
+    public static String formatAsDateTime(LocalDateTime dateTime) {
+        return dateTime.format(YYYY_MM_DD_HH_MM_SS_FORMATTER);
     }
 }
