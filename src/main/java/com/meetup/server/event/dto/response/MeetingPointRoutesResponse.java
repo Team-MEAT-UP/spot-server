8b43b1d1bd7aaf778e0e7ed1b7eb92ad9b3b6c59
syjdjr <syjdjr@naver.com>
2025-08-02 15:01:28 +0900

    CHORE: (SPOT-132) Response 에 @Schema 추가하여 swagger 문서화를 진행한다

--- src/main/java/com/meetup/server/event/dto/response/MeetingPointRoutesResponse.java
+++ src/main/java/com/meetup/server/event/dto/response/MeetingPointRoutesResponse.java
@@ -4,18 +4,32 @@ import com.meetup.server.event.domain.Event;
 import com.meetup.server.global.util.TimeUtil;
 import com.meetup.server.startpoint.domain.StartPoint;
 import com.meetup.server.startpoint.util.UsernameExtractor;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.time.LocalDateTime;
 import java.util.Comparator;
 import java.util.List;
 
 public record MeetingPointRoutesResponse(
+        @Schema(description = "모임명", example = "SPOT 정기회의")
         String eventName,
+
+        @Schema(description = "모임 날짜 (yyyy-MM-dd 형식)", example = "2025-07-09")
         String eventDate,
+
+        @Schema(description = "모임 시간 (HH:mm 형식)", example = "14:00")
         String eventTime,
+
+        @Schema(description = "모임 생성자", example = "홍길동")
         String eventMaker,
+
+        @Schema(description = "확정 장소명", example = "스타벅스 방배카페거리점")
         String placeName,
+
+        @Schema(description = "참여자 수", example = "5")
         int peopleCount,
+
+        @Schema(description = "모임 경로 관련 그룹 데이터")
         List<MeetingPointRouteGroup> meetingPointRouteGroups
 ) {
 
