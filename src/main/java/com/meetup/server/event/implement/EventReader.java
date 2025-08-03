0a62e8d2a54da1c6bd7df5347fcef13ce371416b
syjdjr <syjdjr@naver.com>
2025-08-03 13:48:44 +0900

    FIX: (SPOT-132) readEventCache 메서드 복구한다

--- src/main/java/com/meetup/server/event/implement/EventReader.java
+++ src/main/java/com/meetup/server/event/implement/EventReader.java
@@ -1,10 +1,12 @@
 package com.meetup.server.event.implement;
 
 import com.meetup.server.event.domain.Event;
+import com.meetup.server.event.dto.response.MeetingPointRoutesResponse;
 import com.meetup.server.event.exception.EventErrorType;
 import com.meetup.server.event.exception.EventException;
 import com.meetup.server.event.persistence.EventRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
 import org.springframework.stereotype.Component;
 
@@ -21,5 +23,16 @@ public class EventReader {
     public Event read(UUID eventId) {
         return eventRepository.findById(eventId)
                 .orElseThrow(() -> new EventException(EventErrorType.EVENT_NOT_FOUND));
+    }
+
+    /**
+     * #139 캐시 데이터 가져올 때 사용하는 메서드
+     */
+    public MeetingPointRoutesResponse readEventCache(UUID eventId) {
+        Cache cache = cacheManager.getCache("routeDetails");
+        Cache.ValueWrapper wrapper = cache.get(eventId);
+
+        eventValidator.validateEventCacheExist(wrapper);
+        return (MeetingPointRoutesResponse) wrapper.get();
     }
 }
