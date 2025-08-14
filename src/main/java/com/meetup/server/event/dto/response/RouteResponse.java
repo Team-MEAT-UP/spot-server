85e0069312e5e83337f3475f2223bc8eeac47438
syjdjr <syjdjr@naver.com>
2025-08-14 09:04:11 +0900

    FEAT: (SPOT-161) 법정동 분기를 늘린다

--- src/main/java/com/meetup/server/event/dto/response/RouteResponse.java
+++ src/main/java/com/meetup/server/event/dto/response/RouteResponse.java
@@ -20,8 +20,8 @@ import java.util.regex.Pattern;
 @AllArgsConstructor
 public class RouteResponse {
 
-    private static final Pattern START_POINT_FULL_ADDRESS_PATTERN = Pattern.compile("(\\S+(구|군))\\s+(\\S+(동|읍))");
-    private static final Pattern START_POINT_REGION_ONLY_PATTERN = Pattern.compile("(\\S+(시|군|구))");
+    private static final Pattern FULL_ADDRESS_PATTERN = Pattern.compile("(\\S+(시|군|구))(\\s+\\S+(구|군))?\\s+(\\S+(동|읍|면|가|로))");
+    private static final Pattern REGION_ONLY_PATTERN = Pattern.compile("(\\S+(시|군|구))");
 
     private Boolean isTransit;  // true: 대중교통, false: 자동차
     private Boolean isMe;
@@ -64,15 +64,45 @@ public class RouteResponse {
     private static String convertStartPointName(String address) {
         if (address == null || address.isBlank()) return "";
 
-        Matcher matcher = START_POINT_FULL_ADDRESS_PATTERN.matcher(address);
-        if (matcher.find()) {
-            return matcher.group(1) + " " + matcher.group(3);
-        } else {
-            Matcher siGunGuMatcher = START_POINT_REGION_ONLY_PATTERN.matcher(address);
-            if (siGunGuMatcher.find()) {
-                return siGunGuMatcher.group(1);
+        Matcher fullMatcher = FULL_ADDRESS_PATTERN.matcher(address);
+        Matcher regionMatcher = REGION_ONLY_PATTERN.matcher(address);
+
+        if (fullMatcher.find()) {
+            StringBuilder sb = new StringBuilder();
+
+            String siGunGu = fullMatcher.group(1).trim();
+            String gu = fullMatcher.group(3) != null ? fullMatcher.group(3).trim() : null;
+            String dongEupMyeonGaRo = fullMatcher.group(5).trim();
+
+            if (siGunGu.endsWith("구") && gu == null) {
+                return sb.append(siGunGu).append(" ")
+                        .append(dongEupMyeonGaRo)
+                        .toString();
             }
+
+            if (siGunGu.endsWith("시") && gu == null) {
+                return sb.append(siGunGu).append(" ")
+                        .append(dongEupMyeonGaRo)
+                        .toString();
+            }
+
+            if (siGunGu.endsWith("시") && gu != null) {
+                return sb.append(siGunGu).append(" ")
+                        .append(gu).append(" ")
+                        .append(dongEupMyeonGaRo)
+                        .toString();
+            }
+
+            return sb.append(siGunGu).append(" ")
+                    .append(gu != null ? gu + " " : "")
+                    .append(dongEupMyeonGaRo)
+                    .toString();
         }
+
+        if (regionMatcher.find()) {
+            return regionMatcher.group(1).trim();
+        }
+
         return "";
     }
 
