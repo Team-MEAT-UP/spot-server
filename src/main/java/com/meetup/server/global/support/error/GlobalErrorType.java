827632ef75852464e85aa4789d9151e24fda6ad5
anxi01 <tjdals2893@naver.com>
2025-09-10 17:19:44 +0900

    REFACTOR: (MOISAM-98) 불필요한 에러 노이즈는 디스코드 알림 전송하지 않도록 변경한다

--- src/main/java/com/meetup/server/global/support/error/GlobalErrorType.java
+++ src/main/java/com/meetup/server/global/support/error/GlobalErrorType.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 public enum GlobalErrorType implements ErrorType {
 
     INTERNAL_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "알 수 없는 내부 오류입니다."),
+    NOT_FOUND_RESOURCE(HttpStatus.NOT_FOUND, "존재하지 않는 자원입니다."),
     FAILED_REQUEST_VALIDATION(HttpStatus.BAD_REQUEST, "요청 데이터 검증에 실패하였습니다."),
     INVALID_REQUEST_ARGUMENT(HttpStatus.BAD_REQUEST, "잘못된 요청 인자입니다."),
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "인증에 실패하였습니다."),
