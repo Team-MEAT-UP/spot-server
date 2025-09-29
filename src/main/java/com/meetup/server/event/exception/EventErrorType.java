25d292dd4cffbfb43004334a970fa8f98bd93c77
SeongMin Han <123073840+anxi01@users.noreply.github.com>
2025-09-29 13:45:36 +0900

    [MOISAM-218] Thread를 사용하여 다중 출발지의 경로 병렬 처리하기 (#160)
    * FEAT: (MOISAM-218) Virtual Thread를 사용하여 경로 조회를 병렬처리한다
    
    * REFACTOR: (MOISAM-218) 클래스명 변경 및 경로 페칭하는 로직을 옮긴다
    
    * FEAT: (MOISAM-218) CompletableFuture를 사용해 비동기 처리 방식을 변경한다
    
    * FEAT: (MOISAM-218) 플랫폼 스레드 2개로 관리하도록 변경한다
    
    * REFACTOR: (MOISAM-218) 매직넘버를 상수로 변경한다
    
    * TEST: (MOISAM-218) 출발지 개수에 따른 경로 생성 소요 시간 측정 테스트를 작성한다

--- src/main/java/com/meetup/server/event/exception/EventErrorType.java
+++ src/main/java/com/meetup/server/event/exception/EventErrorType.java
@@ -14,7 +14,7 @@ public enum EventErrorType implements ErrorType {
     SAME_SUBWAY_FOR_ALL_START_POINTS(HttpStatus.BAD_REQUEST, "모든 출발지의 지하철역이 동일합니다."),
     NO_INTERMEDIATE_SUBWAYS_FOUND(HttpStatus.NOT_FOUND, "중간 지점 근처에 지하철역을 찾을 수 없습니다."),
     PATH_CALCULATION_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "중간 지점 경로 계산에 실패했습니다."),
-    NOT_FOUND_ROUTE_INFO(HttpStatus.NOT_FOUND, "경로 조회에 실패했습니다."),
+    ROUTE_FETCH_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "경로 조회에 실패했습니다."),
     CACHE_NOT_FOUND(HttpStatus.NOT_FOUND, "캐시가 존재하지 않습니다.")
     ;
 
