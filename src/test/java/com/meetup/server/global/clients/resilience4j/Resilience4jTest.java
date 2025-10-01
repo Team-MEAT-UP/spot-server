5bc094c1f3ff664327ae5b22b84b1a4ff2f2c573
anxi01 <tjdals2893@naver.com>
2025-10-01 17:19:42 +0900

    TEST: (MOISAM-228) Client 기준으로 @Nested 클래스를 나눠 테스트를 수행한다

--- src/test/java/com/meetup/server/global/clients/resilience4j/Resilience4jTest.java
+++ src/test/java/com/meetup/server/global/clients/resilience4j/Resilience4jTest.java
@@ -15,6 +15,7 @@ import io.github.resilience4j.retry.Retry;
 import io.github.resilience4j.retry.RetryConfig;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.web.client.ResourceAccessException;
@@ -34,8 +35,18 @@ public class Resilience4jTest extends IntegrationTestContainer {
     @MockitoBean
     private OdsayTransitRouteSearchClient odsayTransitRouteSearchClient;
 
-    private KakaoMobilityRequest kakaoMobilityRequest;
-    private OdsayTransitRouteSearchRequest odsayTransitRouteSearchRequest;
+    private static final KakaoMobilityRequest kakaoMobilityRequest = KakaoMobilityRequest.builder()
+            .origin("127.10764191124568,37.402464820205246")
+            .destination("127.11056336672839,37.39419693653072")
+            .build();
+
+    private static final OdsayTransitRouteSearchRequest odsayTransitRouteSearchRequest = OdsayTransitRouteSearchRequest.builder()
+            .sx("127.10764191124568")
+            .sy("37.402464820205246")
+            .ex("127.11056336672839")
+            .ey("37.39419693653072")
+            .build();
+
     private CircuitBreaker circuitBreaker;
     private Retry retry;
 
@@ -55,141 +66,139 @@ public class Resilience4jTest extends IntegrationTestContainer {
                 .build();
         retry = Retry.of("testRetry", retryConfig);
 
-        kakaoMobilityRequest = KakaoMobilityRequest.builder()
-                .origin("127.10764191124568,37.402464820205246")
-                .destination("127.11056336672839,37.39419693653072")
-                .build();
-
-        odsayTransitRouteSearchRequest = OdsayTransitRouteSearchRequest.builder()
-                .sx("127.10764191124568")
-                .sy("37.402464820205246")
-                .ex("127.11056336672839")
-                .ey("37.39419693653072")
-                .build();
-
         circuitBreaker.reset();
     }
 
-    @Test
-    @DisplayName("카카오 모빌리티: 호출 성공 시 서킷 브레이커는 닫힌 상태를 유지한다")
-    void shouldStayWhenCircuitIsClosedOnSuccessForKakaoMobility() {
-        // given
-        circuitBreaker.transitionToClosedState();
-        when(kakaoMobilityClient.sendRequest(kakaoMobilityRequest))
-                .thenReturn(new KakaoMobilityResponse("transId", new ArrayList<>()));
+    @Nested
+    @DisplayName("Kakao Mobility Client")
+    class KakaoMobilityClientTest {
 
-        // when
-        assertDoesNotThrow(() -> kakaoMobilityClient.sendRequest(kakaoMobilityRequest));
+        @Test
+        @DisplayName("호출 성공 시 서킷 브레이커는 닫힌 상태를 유지한다")
+        void shouldStayWhenCircuitIsClosedOnSuccessForKakaoMobility() {
+            // given
+            circuitBreaker.transitionToClosedState();
+            when(kakaoMobilityClient.sendRequest(kakaoMobilityRequest))
+                    .thenReturn(new KakaoMobilityResponse("transId", new ArrayList<>()));
 
-        // then
-        assertEquals(circuitBreaker.getState(), CircuitBreaker.State.CLOSED);
-        verify(kakaoMobilityClient, times(1)).sendRequest(kakaoMobilityRequest);
+            // when
+            assertDoesNotThrow(() -> circuitBreaker.executeSupplier(() -> kakaoMobilityClient.sendRequest(kakaoMobilityRequest)));
+
+            // then
+            assertEquals(circuitBreaker.getState(), CircuitBreaker.State.CLOSED);
+            verify(kakaoMobilityClient, times(1)).sendRequest(kakaoMobilityRequest);
+        }
+
+        @Test
+        @DisplayName("타임아웃 발생 시, 재시도 로직이 실행된 후 실패한다")
+        void shouldRetryOnTimeoutAndFailForKakaoMobility() {
+            // given
+            when(kakaoMobilityClient.sendRequest(kakaoMobilityRequest))
+                    .thenThrow(new ResourceAccessException("Read timed out"));
+
+            // when
+            Supplier<KakaoMobilityResponse> supplierWithFallback = () -> {
+                try {
+                    return retry.executeSupplier(() -> kakaoMobilityClient.sendRequest(kakaoMobilityRequest));
+                } catch (Throwable t) {
+                    throw new ClientException(ClientErrorType.KAKAO_MOBILITY_SERVICE_UNAVAILABLE);
+                }
+            };
+
+            // then
+            assertThrows(ClientException.class, supplierWithFallback::get);
+            verify(kakaoMobilityClient, times(3)).sendRequest(kakaoMobilityRequest);
+        }
+
+        @Test
+        @DisplayName("서킷 브레이커가 열린 상태일 때 호출은 즉시 실패한다")
+        void shouldFailImmediatelyWhenCircuitIsOpenForKakaoMobility() {
+            // given
+            circuitBreaker.transitionToOpenState();
+            when(kakaoMobilityClient.sendRequest(kakaoMobilityRequest))
+                    .thenThrow(new ResourceAccessException("Read timed out"));
+
+            // when
+            Supplier<KakaoMobilityResponse> supplierWithFallback = () -> {
+                try {
+                    return circuitBreaker.executeSupplier(() -> kakaoMobilityClient.sendRequest(kakaoMobilityRequest));
+                } catch (Throwable t) {
+                    throw new ClientException(ClientErrorType.KAKAO_MOBILITY_SERVICE_UNAVAILABLE);
+                }
+            };
+
+            // then
+            assertThrows(ClientException.class, supplierWithFallback::get);
+            assertEquals(circuitBreaker.getState(), CircuitBreaker.State.OPEN);
+            verify(kakaoMobilityClient, never()).sendRequest(kakaoMobilityRequest);
+        }
     }
 
-    @Test
-    @DisplayName("Odsay: 호출 성공 시 서킷 브레이커는 닫힌 상태를 유지한다")
-    void shouldStayWhenCircuitIsClosedOnSuccessForOdsay() {
-        // given
-        circuitBreaker.transitionToClosedState();
-        when(odsayTransitRouteSearchClient.sendRequest(odsayTransitRouteSearchRequest))
-                .thenReturn(new OdsayTransitRouteSearchResponse(
-                        new OdsayTransitRouteSearchResponse.TransitData(
-                                0, 0, 0, 0, 0, 0, 0, 0, new ArrayList<>()
-                        )));
+    @Nested
+    @DisplayName("Odsay Client")
+    class OdsayClientTest {
 
-        // when
-        assertDoesNotThrow(() -> odsayTransitRouteSearchClient.sendRequest(odsayTransitRouteSearchRequest));
+        @Test
+        @DisplayName("호출 성공 시 서킷 브레이커는 닫힌 상태를 유지한다")
+        void shouldStayWhenCircuitIsClosedOnSuccessForOdsay() {
+            // given
+            circuitBreaker.transitionToClosedState();
+            when(odsayTransitRouteSearchClient.sendRequest(odsayTransitRouteSearchRequest))
+                    .thenReturn(new OdsayTransitRouteSearchResponse(
+                            new OdsayTransitRouteSearchResponse.TransitData(
+                                    0, 0, 0, 0, 0, 0, 0, 0, new ArrayList<>()
+                            )));
 
-        // then
-        assertEquals(circuitBreaker.getState(), CircuitBreaker.State.CLOSED);
-        verify(odsayTransitRouteSearchClient, times(1)).sendRequest(odsayTransitRouteSearchRequest);
-    }
+            // when
+            assertDoesNotThrow(() -> circuitBreaker.executeSupplier(() -> odsayTransitRouteSearchClient.sendRequest(odsayTransitRouteSearchRequest)));
 
-    @Test
-    @DisplayName("카카오 모빌리티: 타임아웃 발생 시, 재시도 로직이 실행된 후 실패한다")
-    void shouldRetryOnTimeoutAndFailForKakaoMobility() {
-        // given
-        when(kakaoMobilityClient.sendRequest(kakaoMobilityRequest))
-                .thenThrow(new ResourceAccessException("Read timed out"));
+            // then
+            assertEquals(circuitBreaker.getState(), CircuitBreaker.State.CLOSED);
+            verify(odsayTransitRouteSearchClient, times(1)).sendRequest(odsayTransitRouteSearchRequest);
+        }
 
-        // when
-        Supplier<KakaoMobilityResponse> supplierWithFallback = () -> {
-            try {
-                return retry.executeSupplier(() -> kakaoMobilityClient.sendRequest(kakaoMobilityRequest));
-            } catch (Throwable t) {
-                throw new ClientException(ClientErrorType.KAKAO_MOBILITY_SERVICE_UNAVAILABLE);
-            }
-        };
+        @Test
+        @DisplayName("타임아웃 발생 시, 재시도 로직이 실행된 후 실패한다")
+        void shouldRetryOnTimeoutAndFailForOdsay() {
+            // given
+            when(odsayTransitRouteSearchClient.sendRequest(odsayTransitRouteSearchRequest))
+                    .thenThrow(new ResourceAccessException("Read timed out"));
 
-        // then
-        assertThrows(ClientException.class, supplierWithFallback::get);
-        verify(kakaoMobilityClient, times(3)).sendRequest(kakaoMobilityRequest);
-    }
+            // when
+            Supplier<OdsayTransitRouteSearchResponse> supplierWithFallback = () -> {
+                try {
+                    return retry.executeSupplier(() -> odsayTransitRouteSearchClient.sendRequest(odsayTransitRouteSearchRequest));
+                } catch (Throwable t) {
+                    throw new ClientException(ClientErrorType.ODSAY_SERVICE_UNAVAILABLE);
+                }
+            };
 
-    @Test
-    @DisplayName("Odsay: 타임아웃 발생 시, 재시도 로직이 실행된 후 실패한다")
-    void shouldRetryOnTimeoutAndFailForOdsay() {
-        // given
-        when(odsayTransitRouteSearchClient.sendRequest(odsayTransitRouteSearchRequest))
-                .thenThrow(new ResourceAccessException("Read timed out"));
+            // then
+            assertThrows(ClientException.class, supplierWithFallback::get);
+            verify(odsayTransitRouteSearchClient, times(3)).sendRequest(odsayTransitRouteSearchRequest);
+        }
 
-        // when
-        Supplier<OdsayTransitRouteSearchResponse> supplierWithFallback = () -> {
-            try {
-                return retry.executeSupplier(() -> odsayTransitRouteSearchClient.sendRequest(odsayTransitRouteSearchRequest));
-            } catch (Throwable t) {
-                throw new ClientException(ClientErrorType.ODSAY_SERVICE_UNAVAILABLE);
-            }
-        };
+        @Test
+        @DisplayName("서킷 브레이커가 열린 상태일 때 호출은 즉시 실패한다")
+        void shouldFailImmediatelyWhenCircuitIsOpenForOdsay() {
+            // given
+            circuitBreaker.transitionToOpenState();
+            when(odsayTransitRouteSearchClient.sendRequest(odsayTransitRouteSearchRequest))
+                    .thenThrow(new ResourceAccessException("Read timed out"));
 
-        // then
-        assertThrows(ClientException.class, supplierWithFallback::get);
-        verify(odsayTransitRouteSearchClient, times(3)).sendRequest(odsayTransitRouteSearchRequest);
-    }
+            // when
+            Supplier<OdsayTransitRouteSearchResponse> supplierWithFallback = () -> {
+                try {
+                    return circuitBreaker.executeSupplier(() -> odsayTransitRouteSearchClient.sendRequest(odsayTransitRouteSearchRequest));
+                } catch (Throwable t) {
+                    throw new ClientException(ClientErrorType.ODSAY_SERVICE_UNAVAILABLE);
+                }
+            };
 
-    @Test
-    @DisplayName("카카오 모빌리티: 서킷 브레이커가 열린 상태일 때 호출은 즉시 실패한다")
-    void shouldFailImmediatelyWhenCircuitIsOpenForKakaoMobility() {
-        // given
-        circuitBreaker.transitionToOpenState();
-        when(kakaoMobilityClient.sendRequest(kakaoMobilityRequest))
-                .thenThrow(new ResourceAccessException("Read timed out"));
-
-        // when
-        Supplier<KakaoMobilityResponse> supplierWithFallback = () -> {
-            try {
-                return circuitBreaker.executeSupplier(() -> kakaoMobilityClient.sendRequest(kakaoMobilityRequest));
-            } catch (Throwable t) {
-                throw new ClientException(ClientErrorType.KAKAO_MOBILITY_SERVICE_UNAVAILABLE);
-            }
-        };
-
-        // then
-        assertThrows(ClientException.class, supplierWithFallback::get);
-        assertEquals(circuitBreaker.getState(), CircuitBreaker.State.OPEN);
-        verify(kakaoMobilityClient, never()).sendRequest(kakaoMobilityRequest);
-    }
-
-    @Test
-    @DisplayName("Odsay: 서킷 브레이커가 열린 상태일 때 호출은 즉시 실패한다")
-    void shouldFailImmediatelyWhenCircuitIsOpenForOdsay() {
-        // given
-        circuitBreaker.transitionToOpenState();
-        when(odsayTransitRouteSearchClient.sendRequest(odsayTransitRouteSearchRequest))
-                .thenThrow(new ResourceAccessException("Read timed out"));
-
-        // when
-        Supplier<OdsayTransitRouteSearchResponse> supplierWithFallback = () -> {
-            try {
-                return circuitBreaker.executeSupplier(() -> odsayTransitRouteSearchClient.sendRequest(odsayTransitRouteSearchRequest));
-            } catch (Throwable t) {
-                throw new ClientException(ClientErrorType.ODSAY_SERVICE_UNAVAILABLE);
-            }
-        };
-
-        // then
-        assertThrows(ClientException.class, supplierWithFallback::get);
-        assertEquals(circuitBreaker.getState(), CircuitBreaker.State.OPEN);
-        verify(odsayTransitRouteSearchClient, never()).sendRequest(odsayTransitRouteSearchRequest);
+            // then
+            assertThrows(ClientException.class, supplierWithFallback::get);
+            assertEquals(circuitBreaker.getState(), CircuitBreaker.State.OPEN);
+            verify(odsayTransitRouteSearchClient, never()).sendRequest(odsayTransitRouteSearchRequest);
+        }
     }
 }
