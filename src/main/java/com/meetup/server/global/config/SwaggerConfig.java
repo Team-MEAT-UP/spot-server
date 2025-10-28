4bfe91656a302d0ce8fecb971967087cc79647fc
anxi01 <tjdals2893@naver.com>
2025-10-28 19:54:06 +0900

    FEAT: (MOISAM-239) Swagger와 Rest Docs를 통합하는 설정을 추가한다

--- src/main/java/com/meetup/server/global/config/SwaggerConfig.java
+++ src/main/java/com/meetup/server/global/config/SwaggerConfig.java
@@ -1,5 +1,9 @@
 package com.meetup.server.global.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.meetup.server.global.support.error.GlobalErrorType;
+import com.meetup.server.global.support.error.GlobalException;
+import io.swagger.v3.core.util.Json;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.servers.Server;
@@ -9,28 +13,60 @@ import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
+import java.io.InputStream;
 import java.util.Collections;
-@OpenAPIDefinition(
-        info = @Info(title = "SPOT API 명세서",
-                description = "SPOT API 명세서",
-                version = "v1"
-        ),
-        servers = @Server(url = "${springdoc.server-url}", description = "Default Server URL")
-)
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "MOISAM API Specification",
+                description = "OpenAPI 3.0 specification for the MOISAM",
+                version = "v1.1.2"
+        ),
+        servers = @Server(url = "${springdoc.server-url}", description = "Server URL")
+)
 @Configuration
 public class SwaggerConfig {
 
-        @Bean
-        public OpenAPI openAPI() {
-                SecurityScheme securityScheme = new SecurityScheme()
-                        .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
-                        .in(SecurityScheme.In.HEADER).name("Authorization");
-                SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
+    private static final String OAS_FILE_PATH = "static/swagger-ui/openapi3.json";
+    private static final String SECURITY_SCHEME_NAME = "BearerAuth";
 
-                return new OpenAPI()
-                        .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
-                        .security(Collections.singletonList(securityRequirement));
+    @Bean
+    public OpenAPI openAPI() {
+        OpenAPI openAPI = new OpenAPI();
+
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization");
+
+        ObjectMapper swaggerMapper = Json.mapper();
+        ClassPathResource resource = new ClassPathResource(OAS_FILE_PATH);
+
+        try (InputStream inputStream = resource.getInputStream()) {
+            OpenAPI restDocsOpenAPI = swaggerMapper.readValue(inputStream, OpenAPI.class);
+
+            Components components = restDocsOpenAPI.getComponents() != null
+                    ? restDocsOpenAPI.getComponents()
+                    : new Components();
+
+            components.addSecuritySchemes(SECURITY_SCHEME_NAME, securityScheme);
+            openAPI.components(components);
+
+            if (restDocsOpenAPI.getPaths() != null) {
+                openAPI.setPaths(restDocsOpenAPI.getPaths());
+            }
+
+            SecurityRequirement securityRequirement = new SecurityRequirement().addList(SECURITY_SCHEME_NAME);
+            openAPI.security(Collections.singletonList(securityRequirement));
+
+        } catch (Exception e) {
+            throw new GlobalException(GlobalErrorType.FAILED_SWAGGER_REST_DOCS_INTEGRATION);
         }
+
+        return openAPI;
+    }
 }
