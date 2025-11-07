d91b4ae1effb7c84bd1a75ee4d4c3811daeed071
anxi01 <tjdals2893@naver.com>
2025-11-07 16:19:47 +0900

    FEAT: (MOISAM-241) Gzip용 ServletOutputStream을 추가한다

---
+++ src/main/java/com/meetup/server/global/filter/GzipServletOutputStream.java
@@ -0,0 +1,42 @@
+package com.meetup.server.global.filter;
+
+import jakarta.servlet.ServletOutputStream;
+import jakarta.servlet.WriteListener;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.zip.GZIPOutputStream;
+
+public class GzipServletOutputStream extends ServletOutputStream {
+
+    private final GZIPOutputStream gzipOutputStream;
+
+    public GzipServletOutputStream(OutputStream outputStream) throws IOException {
+        this.gzipOutputStream = new GZIPOutputStream(outputStream);
+    }
+
+    @Override
+    public boolean isReady() {
+        return true;
+    }
+
+    @Override
+    public void setWriteListener(WriteListener writeListener) {
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+        gzipOutputStream.write(b);
+    }
+
+    @Override
+    public void flush() throws IOException {
+        gzipOutputStream.flush();
+    }
+
+    @Override
+    public void close() throws IOException {
+        gzipOutputStream.finish();
+        gzipOutputStream.close();
+    }
+}
