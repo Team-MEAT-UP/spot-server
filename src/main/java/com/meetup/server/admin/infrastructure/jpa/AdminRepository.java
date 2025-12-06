02469c74cdff7519c08ad1602c833c49d971059d
anxi01 <tjdals2893@naver.com>
2025-12-07 01:16:03 +0900

    FEAT: (MOISAM-243) Admin을 UserDetails로 변경하여 로그인할 수 있게 한다

---
+++ src/main/java/com/meetup/server/admin/infrastructure/jpa/AdminRepository.java
@@ -0,0 +1,11 @@
+package com.meetup.server.admin.infrastructure.jpa;
+
+import com.meetup.server.admin.domain.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AdminRepository extends JpaRepository<Admin, Long> {
+
+    Optional<Admin> findByUsername(String username);
+}
