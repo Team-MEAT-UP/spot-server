7a9e55d5de3c3cc3ccbd13ae2954f546b247000d
anxi01 <tjdals2893@naver.com>
2025-12-25 20:29:21 +0900

    FEAT: (MOISAM-245) 금일 생성된 이벤트 카운팅 로직을 추가한다

--- src/main/java/com/meetup/server/event/infrastructure/jpa/EventRepository.java
+++ src/main/java/com/meetup/server/event/infrastructure/jpa/EventRepository.java
@@ -24,4 +24,6 @@ public interface EventRepository extends JpaRepository<Event, UUID> {
 
     @Query("SELECT e FROM Event e JOIN FETCH e.place WHERE e.eventDateTime = :eventDateTime")
     List<Event> findAllByEventDateTimeWithPlace(@Param("eventDateTime") LocalDateTime eventDateTime);
+
+    Long countByCreatedAtBetween(LocalDateTime startDateTime, LocalDateTime endDateTime);
 }
