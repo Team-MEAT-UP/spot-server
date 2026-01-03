9d7835f2ce6e5456f471d0e4b7bf8e3153db1651
anxi01 <tjdals2893@naver.com>
2026-01-03 19:16:47 +0900

    FEAT: (MOISAM-246) 로그인 이력 저장 실패 시 로깅을 수행한다

--- src/main/java/com/meetup/server/user/implement/LogUserLoginWriter.java
+++ src/main/java/com/meetup/server/user/implement/LogUserLoginWriter.java
@@ -4,9 +4,13 @@ import com.meetup.server.user.domain.LogUserLogin;
 import com.meetup.server.user.domain.type.LoginStatus;
 import com.meetup.server.user.infrastructure.jpa.LogUserLoginRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
+import java.util.concurrent.CompletableFuture;
+
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class LogUserLoginWriter {
@@ -14,15 +18,20 @@ public class LogUserLoginWriter {
     private final LogUserLoginRepository logUserLoginRepository;
 
     @Async
-    public void save(Long userId, LoginStatus loginStatus, String ipAddress, String userAgent, String failReason) {
-        logUserLoginRepository.save(
-                LogUserLogin.builder()
-                        .userId(userId)
-                        .loginStatus(loginStatus)
-                        .ipAddress(ipAddress)
-                        .userAgent(userAgent)
-                        .failReason(failReason)
-                        .build()
-        );
+    public CompletableFuture<Void> save(Long userId, LoginStatus loginStatus, String ipAddress, String userAgent, String failReason) {
+        try {
+            logUserLoginRepository.save(
+                    LogUserLogin.builder()
+                            .userId(userId)
+                            .loginStatus(loginStatus)
+                            .ipAddress(ipAddress)
+                            .userAgent(userAgent)
+                            .failReason(failReason)
+                            .build()
+            );
+        } catch (Exception e) {
+            log.error("[LogUserLoginWriter]: 유저 로그인 로그 저장에 실패했습니다. userId: {}", userId, e);
+        }
+        return CompletableFuture.completedFuture(null);
     }
 }
