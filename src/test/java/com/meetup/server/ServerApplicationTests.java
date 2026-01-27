9588aa672dc2c06fbb04933d749801942659f289
anxi01 <tjdals2893@naver.com>
2026-01-28 00:39:31 +0900

    FIX: (MOISAM-248) 통합 테스트 환경에서 contextLoads가 이뤄지도록 롤백한다

--- src/test/java/com/meetup/server/ServerApplicationTests.java
+++ src/test/java/com/meetup/server/ServerApplicationTests.java
@@ -1,8 +1,9 @@
 package com.meetup.server;
 
+import com.meetup.server.support.IntegrationTestContainer;
 import org.junit.jupiter.api.Test;
 
-class ServerApplicationTests {
+class ServerApplicationTests extends IntegrationTestContainer {
 
     @Test
     void contextLoads() {
