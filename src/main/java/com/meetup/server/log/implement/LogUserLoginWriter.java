c1340fd92d38004353f599c66fface141892bde1
anxi01 <tjdals2893@naver.com>
2026-01-27 22:28:08 +0900

    REFACTOR: (MOISAM-248) CompletableFuture 객체를 생성하지 않고 void로 반환한다

--- src/main/java/com/meetup/server/log/implement/LogUserLoginWriter.java
+++ src/main/java/com/meetup/server/log/implement/LogUserLoginWriter.java
@@ -8,8 +8,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
-import java.util.concurrent.CompletableFuture;
-
 @Slf4j
 @Component
 @RequiredArgsConstructor
@@ -18,7 +16,7 @@ public class LogUserLoginWriter {
     private final LogUserLoginRepository logUserLoginRepository;
 
     @Async
-    public CompletableFuture<Void> save(Long userId, LoginStatus loginStatus, String ipAddress, String userAgent, String failReason) {
+    public void save(Long userId, LoginStatus loginStatus, String ipAddress, String userAgent, String failReason) {
         try {
             logUserLoginRepository.save(
                     LogUserLogin.builder()
@@ -32,6 +30,5 @@ public class LogUserLoginWriter {
         } catch (Exception e) {
             log.error("[LogUserLoginWriter]: 유저 로그인 로그 저장에 실패했습니다. userId: {}", userId, e);
         }
-        return CompletableFuture.completedFuture(null);
     }
 }
